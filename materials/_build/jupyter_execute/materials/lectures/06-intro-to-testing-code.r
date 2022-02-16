41110161cf24cf3b16e6813af9f5c575849414ad
Tiffany Timbers <tiffany.timbers@stat.ubc.ca>
2022-02-16 15:29:32 -0800

    rebuilt book with testing example

--- materials/_build/jupyter_execute/materials/lectures/06-intro-to-testing-code.r
+++ materials/_build/jupyter_execute/materials/lectures/06-intro-to-testing-code.r
@@ -119,4 +119,149 @@ fahr_to_celsius <- function(temp) {
     (temp - 32) * 5/9
 }
 
+#' Count class observations
+#'
+#' Creates a new data frame with two columns, 
+#' listing the classes present in the input data frame,
+#' and the number of observations for each class.
+#'
+#' @param data_frame A data frame or data frame extension (e.g. a tibble).
+#' @param class_col unquoted column name of column containing class labels
+#'
+#' @return A data frame with two columns. 
+#'   The first column (named class) lists the classes from the input data frame.
+#'   The second column (named count) lists the number of observations for each class from the input data frame.
+#'   It will have one row for each class present in input data frame.
+#'
+#' @export
+#'
+#' @examples
+#' count_classes(mtcars, am)
+count_classes <- function(data_frame, class_col) {
+  # returns a data frame with two columns: class and count
+}
 
+library(testthat)
+
+test_that("`count_classes` should return a data frame or data frame extension", {
+  # tests to be added here
+})
+
+test_that("`count_classes` should return a data frame, or data frame extension, 
+with the number of rows that corresponds to the number of unique classes 
+in the column passed to `class_col`", {
+  # tests to be added here
+})
+
+test_that("`count_classes` should return a data frame, or data frame extension, 
+whose values in the `count` column correspond to the number of observations 
+for the group in the `class` column from the original data frame", {
+  # tests to be added here
+})
+
+test_that("`count_classes` should throw an error when incorrect types 
+are passed to `data_frame` and `class_col` arguments", {
+  # tests to be added here
+})
+
+# function input for tests
+five_classes_3_obs <- data.frame(class_lables = rep(c("class1", "class2", "class3", "class4", "class5"), 3))
+two_classes_3_obs <- data.frame(class_lables = rep(c("class1", "class2"), 3))
+two_classes_3_and_2_obs <- data.frame(class_lables = c(rep(c("class1", "class2"), 2), "class1"))
+two_classes_3_and_1_obs <- data.frame(class_lables = c(rep("class1", 3), "class2"))
+one_class_3_obs <- data.frame(class_lables = rep("class1", 3))
+empty_df  <- data.frame(class_lables = character(0))
+vector_class_labels <- rep(c("class1", "class2"), 3)
+two_classes_3_obs_as_list <- list(class_lables = rep(c("class1", "class2"), 3))
+
+# expected function output
+five_classes_3_obs_output <- data.frame(class = c("class1", "class2", "class3", "class4", "class5"),
+                                        count = rep(3, 5))
+two_classes_3_obs_output <- data.frame(class = c("class1", "class2"),
+                                count = c(3, 3))
+two_classes_3_and_2_obs_output <- data.frame(class = c("class1", "class2"),
+                                      count = c(3, 2))
+two_classes_3_and_1_obs_output <- data.frame(class = c("class1", "class2"),
+                                      count = c(3, 1))
+one_class_3_obs_output <- data.frame(class = "class1",
+                              count = 3)
+empty_df_output <- data.frame(class = character(0),
+                              count = numeric(0))
+
+test_that("`count_classes` should return a tibble", {
+  expect_s3_class(count_classes(two_classes_3_obs, class_lables), "tibble")
+})
+
+test_that("`count_classes` should return a data frame, or data frame extension, 
+with the number of rows that corresponds to the number of unique classes 
+in the column passed to `class_col`", {
+  expect_equivalent(count_classes(five_classes_3_obs, class_lables), five_classes_3_obs_output)
+  expect_equivalent(count_classes(two_classes_3_obs, class_lables), two_classes_3_obs_output)
+  expect_equivalent(count_classes(one_class_3_obs, class_lables), one_class_3_obs_output)
+  expect_equivalent(count_classes(empty_df, class_lables), empty_df_output)
+})
+
+test_that("`count_classes` should return a data frame, or data frame extension, 
+whose values in the `count` column correspond to the number of observations 
+for the group in the `class` column from the original data frame", {
+  expect_equivalent(count_classes(two_classes_3_and_2_obs, class_lables), two_classes_3_and_2_obs_output)
+  expect_equivalent(count_classes(two_classes_3_and_1_obs, class_lables), two_classes_3_and_1_obs_output)
+})
+
+test_that("`count_classes` should throw an error when incorrect types 
+are passed to `data_frame` and `class_col` arguments", {
+  expect_error(count_classes(two_classes_3_obs, vector_class_labels))
+  expect_error(count_classes(two_classes_3_obs_as_list, class_lables))
+})
+
+#' Count class observations
+#'
+#' Creates a new data frame with two columns, 
+#' listing the classes present in the input data frame,
+#' and the number of observations for each class.
+#'
+#' @param data_frame A data frame or data frame extension (e.g. a tibble).
+#' @param class_col unquoted column name of column containing class labels
+#'
+#' @return A data frame with two columns. 
+#'   The first column (named class) lists the classes from the input data frame.
+#'   The second column (named count) lists the number of observations for each class from the input data frame.
+#'   It will have one row for each class present in input data frame.
+#'
+#' @export
+#'
+#' @examples
+#' count_classes(mtcars, am)
+count_classes <- function(data_frame, class_col) {
+  # returns a data frame with two columns: class and count
+  data_frame |>
+    dplyr::group_by({{ class_col }}) |>
+    dplyr::summarize(count = n()) |>
+    dplyr::rename_at(1, ~ "class")
+}
+
+test_that("`count_classes` should return a tibble", {
+  expect_s3_class(count_classes(two_classes_3_obs, class_lables), "data.frame")
+})
+
+test_that("`count_classes` should return a data frame, or data frame extension, 
+with the number of rows that corresponds to the number of unique classes 
+in the column passed to `class_col`", {
+  expect_equivalent(count_classes(five_classes_3_obs, class_lables), five_classes_3_obs_output)
+  expect_equivalent(count_classes(two_classes_3_obs, class_lables), two_classes_3_obs_output)
+  expect_equivalent(count_classes(one_class_3_obs, class_lables), one_class_3_obs_output)
+  expect_equivalent(count_classes(empty_df, class_lables), empty_df_output)
+})
+
+test_that("`count_classes` should return a data frame, or data frame extension, 
+whose values in the `count` column correspond to the number of observations 
+for the group in the `class` column from the original data frame", {
+  expect_equivalent(count_classes(two_classes_3_and_2_obs, class_lables), two_classes_3_and_2_obs_output)
+  expect_equivalent(count_classes(two_classes_3_and_1_obs, class_lables), two_classes_3_and_1_obs_output)
+})
+
+test_that("`count_classes` should throw an error when incorrect types 
+are passed to `data_frame` and `class_col` arguments", {
+  expect_error(count_classes(two_classes_3_obs, vector_class_labels))
+  expect_error(count_classes(two_classes_3_obs_as_list, class_lables))
+})
